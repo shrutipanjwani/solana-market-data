f46e3ad1b007f59282a3ea3f3501be8ea526d680
“shrutipanjwani” <hello@shrutipanjwani.com>
2024-09-20 18:51:32 +0530

    completed additional requirements

--- components/ExchangePage/details.tsx
+++ components/ExchangePage/details.tsx
@@ -4,6 +4,14 @@ import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import { MarketData } from "@/data/markets";
 import Image from "next/image";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
 
 const ExchangeDetails: React.FC<{ exchangeData: MarketData }> = ({
   exchangeData,
@@ -79,6 +87,49 @@ const ExchangeDetails: React.FC<{ exchangeData: MarketData }> = ({
                 </p>
               </div>
             </div>
+          </div>
+        </CardContent>
+      </Card>
+      <Card className="bg-background border-none">
+        <CardContent className="p-6">
+          <h2 className="text-xl font-semibold text-gray-100 mb-4">Markets</h2>
+          <div className="overflow-x-auto">
+            <Table>
+              <TableHeader className="h-[50px]">
+                <TableRow className="rounded-full bg-[#151515]">
+                  <TableHead className="text-gray-300 text-left rounded-l-full pl-4">
+                    NAME
+                  </TableHead>
+                  <TableHead className="text-left text-gray-300">
+                    PAIRS
+                  </TableHead>
+                  <TableHead className="text-left text-gray-300 rounded-r-full">
+                    PRICE
+                  </TableHead>
+                </TableRow>
+              </TableHeader>
+              <TableBody>
+                {exchangeData.pairs.map((pair, index) => (
+                  <TableRow key={index} className="border-b border-gray-800">
+                    <TableCell className="text-blue-400 pl-4">
+                      {`${exchangeData.name}_${pair.pair.replace(
+                        "/",
+                        ""
+                      )}_d.csv`}
+                    </TableCell>
+                    <TableCell className="text-gray-300">
+                      {`${pair.pair}`}
+                    </TableCell>
+                    <TableCell className="text-gray-300">
+                      <span className="flex items-center">
+                        <span className="text-gray-300">$</span>
+                        {pair.price.toFixed(2)}
+                      </span>
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
           </div>
         </CardContent>
       </Card>
